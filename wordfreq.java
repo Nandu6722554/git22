46b35edabdfc355be1546fa53338a733bbb9d5a2
Nandha <kukunoornandu@gmail.com>
2026-06-12 19:58:36 +0530

    Add duplicate 'banana' to the list for frequency count

--- wordfreq.java
+++ wordfreq.java
@@ -3,7 +3,7 @@ import java.util.stream.*;
 
 public class wordfreq {
     public static void main(String[] args) {
-        List<String> lst = Arrays.asList("apple", "banana", "apple", "orange");
+        List<String> lst = Arrays.asList("apple", "banana", "apple", "orange","banana");
         Map<String, Long> freq = lst.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
         System.out.println(freq);
     }
